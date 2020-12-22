aa811b1c40bc85b398999168324eac42826cd4ae
Dmitry Zagorevskiy <7281015@gmail.com>
2020-12-22 11:31:23 +0100

    Lesson 4

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def posts_list(request):
-    n = 'First name'
-    names = {'name': n}
-    return render(request, 'blog/index.html', names)
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
