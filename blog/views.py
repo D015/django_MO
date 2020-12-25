e6c5b9ce8a4d208419f27a2597d034b493f296ea
Dmitry Zagorevskiy <7281015@gmail.com>
2020-12-25 11:05:51 +0100

    Lesson 14

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views.generic import View
@@ -13,7 +14,14 @@ from django.core.paginator import Paginator
 
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(
+            Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 2)
 
     page_number = request.GET.get('page', 1)
